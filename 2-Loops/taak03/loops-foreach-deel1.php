32f55ee48ff4dd7f5ee7281281f9122c15b56bd0
Lorenzo Breedveld <2105144@talnet.nl>
2020-06-11 14:38:49 +0200

    af

---
+++ 2-Loops/taak03/loops-foreach-deel1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+  
+
+
+   <?php
+
+$dagen = array("maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag");
+
+foreach($dagen as $days) {
+    echo $days;
+    echo"<br>";
+}
+
+    
+
+?>
+
+</body>
+
