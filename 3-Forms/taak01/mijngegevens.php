5d7dc522d21ed0ada17c15940e9dba5be17563ce
Lorenzo Breedveld <2105144@talnet.nl>
2020-06-12 14:55:07 +0200

    bijna

--- 3-Forms/taak01/mijngegevens.php
+++ 3-Forms/taak01/mijngegevens.php
@@ -1,11 +1,12 @@
 <html>
 <body>
 
- <?php echo $_POST["naam"]; ?><br>
- <?php echo $_POST["Achternaam"]; ?>
- <?php echo $_POST["klas"]; ?>
- <?php echo $_POST["leeftijd"]; ?>
- <?php echo $_POST["adres"]; ?>
- <?php echo $_POST["plaatsnaam"]; ?>
+ <?php echo $_POST["naam"]; <br>
+  echo $_POST["Achternaam"];
+  echo $_POST["klas"]; 
+ echo $_POST["leeftijd"]; 
+ echo $_POST["adres"];
+  echo $_POST["plaatsnaam"];
+  ?>
 </body>
 </html>
