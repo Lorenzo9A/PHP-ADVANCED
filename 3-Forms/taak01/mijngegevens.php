4cd965199ccc36f4327cff496b27162e8f3e3b56
Lorenzo Breedveld <2105144@talnet.nl>
2020-06-12 14:42:08 +0200

    progress

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+ <?php echo $_POST["naam"]; ?><br>
+ <?php echo $_POST["Achternaam"]; ?>
+ <?php echo $_POST["klas"]; ?>
+ <?php echo $_POST["leeftijd"]; ?>
+ <?php echo $_POST["adres"]; ?>
+ <?php echo $_POST["plaatsnaam"]; ?>
+</body>
+</html>
