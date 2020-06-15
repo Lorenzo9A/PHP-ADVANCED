38be73c5d9a48eba722b82852618cf210141fe56
Lorenzo Breedveld <2105144@talnet.nl>
2020-06-15 15:51:35 +0200

    af

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+</head>
+<body>
+
+ <?php 
+ echo $_GET["artiest"];
+ echo "<br>";
+ echo $_GET["titel"];
+ echo"<br>";
+ echo $_GET["jaarVanUitkomst"];
+ echo"<br>";
+ echo $_GET["muzieksoort"]; 
+ //muziek.nl/mijnmuziek.php?artiest=NLEChoppa&titel=shottaflow&jaarVanUitkomst=2019&muzieksoort=rap
+  ?>
+</body>
+</html>
