346bfd5f9c92cf4e54a9d9faa268ab99a6194823
Lorenzo Breedveld <2105144@talnet.nl>
2020-06-17 13:36:49 +0200

    bijna af

--- opdracht/advanced-2.php
+++ opdracht/advanced-2.php
@@ -3,13 +3,25 @@
     <style>
         body {
             background-color: <?php echo $_POST["kleur"];?>;
+            color: <?php echo $_POST["textkleur"];?>;
         }
     </style>
 </head>
 <body>
-
  <?php 
- 
-  ?>
+function toonTekst(){
+  $mijzelf = array("Naam"=>" Lorenzo", "Achternaam"=>" Breedveld","Leeftijd"=> " 17", "Woonplaats"=>" Amsterdam", "Klas"=> " 9A");
+  foreach($mijzelf as $ik => $gegevens){
+    echo  $ik , $gegevens ; 
+    echo "<br>";
+    }
+} 
+
+?>
+<table>   
+           <tr>
+               <?php toonTekst();?>
+           </tr>
+</table>
 </body>
 </html>
