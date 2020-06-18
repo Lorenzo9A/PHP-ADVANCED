d1b1c795347e362c4057de682b5400c4f017fde5
Lorenzo Breedveld <2105144@talnet.nl>
2020-06-18 10:39:38 +0200

    af

--- opdracht/advanced-2.php
+++ opdracht/advanced-2.php
@@ -1,32 +1,55 @@
 <html>
-  <head>
-</head>
-<body>
-<style>
-        body {
-            background-color: <?php echo $_POST["kleur"];?>;
-            color: <?php echo $_POST["textkleur"];?>;
-        }
-        table{
-          border: <?php echo $_POST["border"]; ?> solid black;
-          padding: <?php echo $_POST["padding"]; ?> 
-        }
 
-    </style>
-  <?php 
-  $mijzelf = array("Naam"=>" Lorenzo", "Achternaam"=>" Breedveld","Leeftijd"=> " 17", "Woonplaats"=>" Amsterdam", "Klas"=> " 9A");
-function maakRij($mijzelf, $table = true){
-  foreach($mijzelf as $ik => $gegevens){
-    echo $ik, $gegevens; 
-    echo "<br>";
+<head>
+</head>
+
+<body>
+  <style>
+    body {
+      background-color: <?php echo $_POST["kleur"]; ?>;
+      color: <?php echo $_POST["textkleur"]; ?>;
+    }
+
+    table,
+    tr,
+    td {
+      border: <?php echo $_POST["border"]; ?> solid black;
+      padding: <?php echo $_POST["padding"]; ?>
+    }
+  </style>
+  <?php
+  $mijzelf = array(
+
+    "Naam"       => " Lorenzo",
+    "Achternaam" => " Breedveld",
+    "Leeftijd"   => " 17",
+    "Woonplaats" => " Amsterdam",
+    "Klas"       => " 9A"
+  );
+
+  function maakRij($mijzelf, $table = true)
+  {
+    foreach ($mijzelf as $ik => $gegevens) {
+      echo "<tr>", "<td>" . $ik .   $gegevens .  "<tr>", "</td>";
+      echo "<br>";
+    }
   }
-} 
-?>
-<table>
-           <tr>
-               <?php maakRij($mijzelf);
-                  ?>
-</tr>
-</table>
+  ?>
+  <table>
+    <thead>
+      <tr>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <?php maakRij($mijzelf);
+        ?>
+      </tr>
+    </tbody>
+  </table>
+
+
+
 </body>
+
 </html>
